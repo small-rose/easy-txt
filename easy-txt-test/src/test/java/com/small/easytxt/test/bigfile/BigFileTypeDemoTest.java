8f9be616f81c336518f374c24115080508722b99
small-rose <small-rose@qq.com>
2022-11-05 19:55:00 +0800

    :memo: add LocalDate LocalDateTime demo

--- easy-txt-test/src/test/java/com/small/easytxt/test/bigfile/BigFileTypeDemoTest.java
+++ easy-txt-test/src/test/java/com/small/easytxt/test/bigfile/BigFileTypeDemoTest.java
@@ -9,7 +9,10 @@ import org.junit.Test;
 import java.io.File;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -20,6 +23,51 @@ import java.util.List;
  * @version: v1.0
  */
 public class BigFileTypeDemoTest {
+
+
+    @Test
+    public void test_write_type(){
+        long start = System.currentTimeMillis();
+        File file = new File("d:\\2_test_type_file.txt");
+        if (file.exists()){ file.delete();}
+        int maxPageNo = 4;
+        for (int i = 1; i <= maxPageNo ; i++) {
+            int finalI = i;
+            EasyTxt.write(file, BigBeanType.class, ",",i, 10, (pageNum, pageSize) -> {
+                //模拟分页数据分批写入文件
+                List<BigBeanType> list = new ArrayList<>(140000);
+                for (int x=1; x <=3 ; x++){
+                    BigBeanType bean = new BigBeanType();
+                    bean.setColumn1('a');
+                    bean.setColumn2((short)2);
+                    bean.setColumn3((byte)3);
+                    bean.setColumn4(4);
+                    bean.setColumn5(5L);
+                    bean.setColumn6(6.6F);
+                    bean.setColumn7(7.7D);
+                    bean.setColumn8(false);
+                    bean.setColumn9('A');
+                    bean.setColumn10((short)10);
+                    bean.setColumn11((byte)11);
+                    bean.setColumn12(12);
+                    bean.setColumn13(13L);
+                    bean.setColumn14(14.14F);
+                    bean.setColumn15(15.15D);
+                    bean.setColumn16(false);
+                    bean.setColumn17(new BigInteger("17"));
+                    bean.setColumn18(new BigDecimal("18.00"));
+                    bean.setColumn19(new Date());
+                    bean.setColumn20("文件操作完毕后必须关闭，否则长期保持对文件的连接状态，造成内存溢出的现象发生");
+                    bean.setColumn21(LocalDate.now());
+                    bean.setColumn22(LocalDateTime.now());
+                    list.add(bean);
+                }
+                return list ;
+
+            }).doWrite();
+            System.out.println(" 文件大小是 "+ FileSizeUtils.fileSizeTransfer(FileSizeUtils.getSize(file.getAbsolutePath())));
+        }
+    }
 
 
     @Test
@@ -53,9 +101,10 @@ public class BigFileTypeDemoTest {
                     bean.setColumn16(false);
                     bean.setColumn17(new BigInteger("17"));
                     bean.setColumn18(new BigDecimal("18.00"));
-                    bean.setColumn19("文件操作完毕后必须关闭，否则长期保持对文件的连接状态，造成内存溢出的现象发生");
+                    bean.setColumn19(new Date());
                     bean.setColumn20("文件操作完毕后必须关闭，否则长期保持对文件的连接状态，造成内存溢出的现象发生");
-
+                    bean.setColumn21(LocalDate.now());
+                    bean.setColumn22(LocalDateTime.now());
                     list.add(bean);
                 }
                 return list ;

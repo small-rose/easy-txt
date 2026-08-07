eb4f209791b8bde87ecb9bf978386ab916713779
smll-rose <smll-rose@qq.com>
2026-08-07 10:13:25 +0800

    :memo: 升级相关功能

--- easy-txt-core/src/main/java/com/small/easytxt/TxtFileWriter.java
+++ easy-txt-core/src/main/java/com/small/easytxt/TxtFileWriter.java
@@ -31,7 +31,7 @@ public class TxtFileWriter extends AbstractFileWriter {
             this.isToBean = true ;
             initBean(object);
         }
-        //ConverterHelper.initConverters(beanFieldMap);
+        ConverterHelper.initConverters(beanFieldMap);
         fileWriteExecutor.execute() ;
 
     }
